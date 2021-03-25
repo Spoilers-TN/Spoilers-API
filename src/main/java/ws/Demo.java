38144582cf4b5f6cdedb99fbaf61e39e3167ab66
Francesco Masala <mail@francescomasala.me>
2021-03-25 18:52:43 +0100

    Updated demo

--- src/main/java/ws/Demo.java
+++ src/main/java/ws/Demo.java
@@ -5,12 +5,19 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/Demo")
+@Path("/demo")
 public class Demo {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String DemoText(){
         return "Prova API Spoilers";
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public String DemoHtml(){
+        return "<html> " + "<title>" + "Spoilers API 0.1" + "</title>"
+                + "<body><h1>" + "Spoilers API 0.1" + "</body></h1>" + "</html> ";
     }
 }
