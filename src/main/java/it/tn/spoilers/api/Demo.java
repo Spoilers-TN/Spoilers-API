15d1a2cf2f66255cb63fdcaf401378841d84a63e
Francesco Masala <mail@francescomasala.me>
2021-03-25 19:42:35 +0100

    Rewritten project

---
+++ src/main/java/it/tn/spoilers/api/Demo.java
@@ -0,0 +1,24 @@
+package it.tn.spoilers.api;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/demo")
+public class Demo {
+	
+	@GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String DemoText(){
+        return "Prova API Spoilers";
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public String DemoHtml(){
+        return "<html> " + "<title>" + "Spoilers API 0.1" + "</title>"
+                + "<body><h1>" + "Spoilers API 0.1" + "</body></h1>" + "</html> ";
+    }
+
+}
